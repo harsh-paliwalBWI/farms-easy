22a6dfce29131ceb4ba2c3c261c93217f049c6e8
Ayush Rawat <rwat905@gmail.com>
2023-09-20 11:29:08 +0530

    'changes'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,25 @@
-import Image from "next/image"
-import LogosSection from '@/components/logosSection/LogosSection'
-import OurMission from '@/components/ourMission/OurMission'
-import HealthierWay from '@/components/healthierWay/HealthierWay'
-import Category from '@/components/category/Category'
-import PlayStoreLink from '@/components/playstoreLink/PlayStoreLink'
-import Testimonials from '@/components/testimonials/Testimonials'
-import CategoryList from '@/components/categoryList/CategoryList'
-import Offers from '@/components/offers/Offers'
-import BestSells from '@/components/bestSells/BestSells'
-import OfferBanner from '@/components/offerBanner/OfferBanner'
-import Features from '@/components/features/Features'
-import Products from '@/components/products/Products'
 // import '../assets/flaticons/flaticon_mycollection.css'
 
-
-// async function getUsers() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
-//   const users = (await res.json());
-//   return users;
-// }
+import { dehydrate } from "@tanstack/react-query";
+import HomeClient from "./HomeClient";
+import { fetchCategories, fetchSubCategories } from "@/utils/databaseService";
+import Hydrate from "@/utils/hydrate.client";
+import getQueryClient from "@/utils/getQueryClient";
 
 export default async function Home() {
-  // const queryClient = getQueryClient();
-  // await queryClient.prefetchQuery(["hydrate-users"], getUsers);
-  // const dehydratedState = dehydrate(queryClient);
+  const client = getQueryClient();
+  await client.prefetchQuery({
+    queryKey: ["categories"],
+    queryFn: fetchCategories,
+  });
+  await client.prefetchQuery({
+    queryKey: ["subCategories"],
+    queryFn: fetchSubCategories,
+  });
+  const dehydratedState = dehydrate(client);
   return (
-    <div className='w-full'>
-      {/* <Hydrate state={dehydratedState}> */}
-      <HealthierWay />
-      {/* </Hydrate> */}
-      <Features/>
-       <Category/>
-      <Products />
-      <OfferBanner/>
-      <BestSells/>
-      <Offers/>
-      <CategoryList/>
-      <Testimonials />
-      <PlayStoreLink />
-      <LogosSection />
-      <OurMission />
-
-    </div>
-  )
+    <Hydrate state={dehydratedState}>
+      <HomeClient />
+    </Hydrate>
+  );
 }
--- src/components/category/Category.tsx
+++ src/components/category/Category.tsx
@@ -1,12 +1,24 @@
+"use client";
 import React from "react";
 import CategoryAvl from "./categoryAvl/CategoryAvl";
 import CategoryCard from "./categoryCard/CategoryCard";
 import img from "../../images/slider-bg-vert-1 1.svg";
 import bottomImg from "../../images/slider-bg-vert-1 2.svg";
 import Image from "next/image";
+import { useQuery } from "@tanstack/react-query";
+import { fetchCategories, fetchSubCategories } from "@/utils/databaseService";
 
-const Category = async () => {
+const Category = () => {
   // const [selectedCategory, setSelectedCategory] = useState(0);
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: () => fetchCategories(),
+  });
+  const { data: subCategories } = useQuery({
+    queryKey: ["sub-categories"],
+    queryFn: () => fetchSubCategories(),
+  });
+
   return (
     <div className="bg-category-bg bg-cover bg-no-repeat md:py-[100px] py-[40px] sm:px-[3.5%] px-[7%] relative   ">
       <div className="text-center">
@@ -16,7 +28,7 @@ const Category = async () => {
         <div className="font-bold md:text-3xl sm:text-2xl text-xl md:pb-[30px] sm:pb-[30px]  text-[#253D4E]">
           Explores by Category
         </div>
-        <CategoryAvl/>
+        <CategoryAvl categories={categories} subCategories={subCategories} />
       </div>
       <div className="absolute top-0 left-0   ">
         <Image
@@ -28,8 +40,9 @@ const Category = async () => {
             width: "100vw",
             objectFit: "cover",
             maxWidth: "100%",
-            height: "auto"
-          }} />
+            height: "auto",
+          }}
+        />
       </div>
       <div className="absolute bottom-0 left-0  ">
         <Image
@@ -41,8 +54,9 @@ const Category = async () => {
             width: "100vw",
             objectFit: "cover",
             maxWidth: "100%",
-            height: "auto"
-          }} />
+            height: "auto",
+          }}
+        />
       </div>
     </div>
   );
