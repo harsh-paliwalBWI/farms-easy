8523cb5fce7099533968abe6ae75ad612d0b980a
Ayush Rawat <rwat905@gmail.com>
2023-11-21 17:34:34 +0530

    'changes'

--- src/components/offerBanner/OfferBanner.tsx
+++ src/components/offerBanner/OfferBanner.tsx
@@ -1,32 +1,39 @@
 "use client";
-import React from "react";
-import bannerImg from "../../images/Group 1747.png";
-import sideImg from "../../images/Offer2-img.svg";
-import Image from "next/image";
-import { FiArrowRight } from "react-icons/fi";
-import leftBanner from "../../images/Group 1748.png";
-import sideImg1 from "../../images/Offer1-img.svg";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import SideMenuLogin from "../sidemenulogin/SideMenulogin";
 
 const OfferBanner = () => {
-  const matches = useMediaQuery("(max-width:685px)");
-  const matches2 = useMediaQuery("(max-width:480px)");
+  const [isShowLoginMenu, setShowLoginMenu] = useState(false);
+
+  const handleLoginClick = () => {
+    // setShowLogin(true);
+    setShowLoginMenu(!isShowLoginMenu);
+    document.body.classList.add("no-scroll");
+  };
+
+  const closeLoginMenu = () => {
+    // dispatch(closeLoginModal());
+    setShowLoginMenu(false);
+    document.body.classList.remove("no-scroll");
+  };
 
   return (
     <div className="sm:px-[3.5%] px-[7%]  py-[100px]">
       <div className="flex flex-wrap gap-5  justify-center items-center ">
-        {/* <div className='grid grid-cols-2 gap-5 place-items-center '> */}
-
         <div
           style={{ position: "relative" }}
           className="relative min-w-[90vw] md:min-w-[45vw] md:flex-1 "
         >
           <Image
-            src="https://firebasestorage.googleapis.com/v0/b/bwi-emb-farmacy.appspot.com/o/Offer1.png?alt=media&token=7aef680f-e2be-4935-bcec-fc31fad0be55"
+            src={require("../../images/Buyers.png")}
             width={100}
             height={100}
+            onClick={handleLoginClick}
             layout="responsive"
-            className="w-full h-full"
+            className="w-full h-full hover:cursor-pointer"
             alt="banner"
             style={{
               maxWidth: "100%",
@@ -38,20 +45,32 @@ const OfferBanner = () => {
           style={{ position: "relative" }}
           className="relative min-w-[90vw] md:min-w-[45vw] md:flex-1 "
         >
-          <Image
-            src="https://firebasestorage.googleapis.com/v0/b/bwi-emb-farmacy.appspot.com/o/Offer2.png?alt=media&token=d4d9882c-6821-4a35-920c-c48094c3edb4"
-            width={100}
-            height={100}
-            layout="responsive"
-            className="w-full h-full"
-            alt="banner"
-            style={{
-              maxWidth: "100%",
-              height: "auto",
-            }}
-          />
+          <Link
+            href={"https://bwi-emb-farmacy-vendor.web.app/"}
+            target="_blank"
+          >
+            <Image
+              src={require("../../images/Farmers.png")}
+              width={100}
+              height={100}
+              layout="responsive"
+              className="w-full h-full"
+              alt="banner"
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+            />
+          </Link>
         </div>
       </div>
+      {isShowLoginMenu && (
+        <SideMenuLogin
+          isOpen={isShowLoginMenu}
+          setShowLogin={setShowLoginMenu}
+          onClose={closeLoginMenu}
+        />
+      )}
     </div>
   );
 };
